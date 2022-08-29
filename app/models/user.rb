24bbb3679158b57d304a340947635d7e9303353c
EC2 Default User <ec2-user@ip-172-31-18-24.ap-northeast-1.compute.internal>
2022-08-29 14:02:39 +0000

    [応用課題4]relationshipモデルの再作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,30 +9,21 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :following_user, through: :follower, source: :followed
-  has_many :follower_user, through: :followed, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", depandent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
   validates :name, length: { in: 2..20 }, uniqueness: true
   validates :introduction, length: { maximum: 50}
 
-
-
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-  
+
   def follow(user_id)
-    follower.create(followed_id: user_id)
+    relationships.create(followed_id: user_id)
   end
-  
-  def unfollow(user_id)
-    follower.find_by(followed_id: user_id).destroy
-  end
-  
-  def following?(user)
-    following_user.include?(user)
-  end
-  
+
 end
