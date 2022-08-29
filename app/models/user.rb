80e6e39f141c82dcd187af6e5d62ddb37b0fce59
EC2 Default User <ec2-user@ip-172-31-18-24.ap-northeast-1.compute.internal>
2022-08-29 14:11:45 +0000

    [応用課題4]relationshipsコントローラの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
 
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", depandent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
